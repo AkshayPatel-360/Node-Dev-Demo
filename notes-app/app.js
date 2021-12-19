cb17ada46bdc7a894c52a72ae5c80c085c232ce1
AkshayPatel-360 <axaymow@gmail.com>
2021-12-18 19:54:55 -0500

    Imported Yargs library and used it

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,46 @@
-require('./util.js')
-const name = 'Akshay'
+const chalk = require('chalk')
+const yargs = require('yargs')
+const getNotes = require('./notes.js')
 
-console.log(name)
+// Customize yargs version 
+
+yargs.version('1.1.0')
+
+// Add, Remove , read ,list
+
+yargs.command({
+        command:'add',
+        describe: 'Add a new note',
+        handler: function(){
+            console.log('Adding a new note')
+        }
+})
+
+yargs.command({
+        command:'remove',
+        describe:'Removing a note',
+        handler: function(){
+            console.log('Removing the note')
+        }
+
+})
+
+
+yargs.command({
+        command:'read',
+        describe:'Reading a  note',
+        handler: function(){
+            console.log('Reading a note')
+        }
+})
+
+yargs.command({
+    command:'list',
+    describe:'Listing out all notes',
+    handler: function(){
+        console.log('Listing a note')
+    }
+})
+
+
+console.log(yargs.argv)
