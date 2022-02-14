4f434000c253cbda7ae4560a1fe1876f2f1ee0ba
AkshayPatel-360 <axaymow@gmail.com>
2022-02-13 23:29:53 -0500

    Create Test.js

---
+++ Test.js
@@ -0,0 +1,20 @@
+
+
+func2()
+const func1 = function(){
+    console.log(xx);
+}
+
+
+function func2(){
+    console.log(funcSquare(10));
+
+}
+
+const square = function(x){
+    return x*x
+}
+
+function funcSquare(x){
+    return x*x
+}
